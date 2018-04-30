c673e5919cc118b1fc05937abee251a9b496ab98
Seungmi Shin <seungmi_shin@tmax.co.kr>
2018-04-30 12:33:01 +0900

    qemu 관련 소스코드git clone shell script  추가

---
+++ clone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/torvalds/linux.git
+git clone https://github.com/mesa3d/mesa.git
+git clone https://github.com/freedesktop/virglrenderer.git
+git clone https://github.com/qemu/qemu.git
